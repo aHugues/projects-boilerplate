c8a90880880e0397dfe7bc698aa7a20cd2b22ce0
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2019-11-23 16:50:24 +0100

    Add entrypoint for the boilerplate
    Add main entrypoint with some arguments to the boilerplate and adjust
    package settings to use this new entrypoint

--- projects_boilerplate/__init__.py
+++ projects_boilerplate/__init__.py
@@ -2,10 +2,4 @@
 Sample module for testing the project.
 """
 
-
-def function1():
-    return 3
-
-
-def function2():
-    return 1
+from .main import main
--- tests/test_sample.py
+++
@@ -1,7 +0,0 @@
-"""
-Second set of tests
-"""
-
-
-def test_sample():
-    assert 1 == 1  # pylint: disable=comparison-with-itself
